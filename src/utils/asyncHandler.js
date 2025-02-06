086432c817876a0fc9864093edb14d76449210b8
Divyam Verma <divyamvbij@gmail.com>
2025-02-06 19:37:10 +0530

    Managing utils

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,19 @@
+const asyncHandler = (fn) => {
+  (req, res, next) => {
+    Promise.resolve(fn(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+//another way using try-catch for same thing
+// const asyncHandler = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (err) {
+//     res.status(err.code).json({
+//       success: false,
+//       message: err.message,
+//     });
+//   }
+// };
