26d3fa5388818cbfc5b0f0d3787b8080ddeac0c1
Divyam Verma <divyamvbij@gmail.com>
2025-02-08 19:47:29 +0530

    first testing configures routes

--- src/app.js
+++ src/app.js
@@ -5,15 +5,19 @@ import cookieParser from "cookie-parser";
 const app = express();
 
 app.use(
-  //cors config
-  cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true,
-  })
+    //cors config
+    cors({
+        origin: process.env.CORS_ORIGIN,
+        credentials: true,
+    })
 );
 app.use(express.json({ limit: "16kb" })); //config to accept json data
 app.use(express.urlencoded({ limit: "16kb", extended: true })); //options in both are not necessary
 app.use(express.static("public"));
 app.use(cookieParser());
+
+import userRoute from "./routes/user.routes.js";
+//routes
+app.use("/api/v1/users", userRoute);
 
 export default app;
--- src/database/index.js
+++ src/database/index.js
@@ -2,15 +2,17 @@ import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
 const connectDb = async () => {
-  try {
-    const connectionInstance = await mongoose.connect(
-      `${process.env.MONGO_URI}/${DB_NAME}`
-    );
-    console.log(`DB connected on host: ${connectionInstance.connection.host}`);
-  } catch (err) {
-    console.error("Connection with database failed");
-    process.exit(1);
-  }
+    try {
+        const connectionInstance = await mongoose.connect(
+            `${process.env.MONGO_URI}/${DB_NAME}`
+        );
+        console.log(
+            `DB connected on host: ${connectionInstance.connection.host}`
+        );
+    } catch (err) {
+        console.error("Connection with database failed");
+        process.exit(1);
+    }
 };
 
 export default connectDb;
