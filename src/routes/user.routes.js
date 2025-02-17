fac7daf74be5b5ba2bed041fc4b85e15becdb168
Divyam Verma <divyamvbij@gmail.com>
2025-02-17 18:49:05 +0530

    Add user routes for password change, avatar update, current user retrieval, user profile, and watch history

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,6 +4,11 @@ import {
     loginUser,
     logoutUser,
     refreshAccessToken,
+    changePassword,
+    getCurrentUser,
+    updateAvatar,
+    getUserProfile,
+    getUserHistory,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
@@ -23,5 +28,12 @@ router.route("/login").post(loginUser);
 //secured routes
 router.route("/logout").post(verifyJwt, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJwt, changePassword);
+router
+    .route("/update-avatar")
+    .patch(verifyJwt, upload.single("avatar"), updateAvatar);
+router.route("/current-user").get(verifyJwt, getCurrentUser);
+router.route("/channel/:username").get(verifyJwt, getUserProfile);
+router.route("/history").get(verifyJwt, getUserHistory);
 
 export default router;
